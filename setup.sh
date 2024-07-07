5a30364cd6ad12e5d55291da7ef346dbeb88bbfb
Hajime Tahara <h.tahara0926@gmail.com>
2024-07-07 23:51:17 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install -y ssh vsftpd chrony vim-gtk3
+sudo apt install -y python3-pip
+sudo apt install -y git curl wget
+sudo apt install -y mosquitto
+sudo apt install -y ibus-mozc mozc-utils-gui
